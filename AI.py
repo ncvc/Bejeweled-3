5f81f6fcbf609b44e592170a5efab4f951aa4d49
Christopher G Dolan <cdolan@mit.edu>
2011-08-17 22:13:47 -0700

    created

---
+++ AI.py
@@ -0,0 +1,11 @@
+def determineMove(board):
+    width = board.length()
+    if !width > 3:
+        exit()
+    height = board[0].length()
+    if !width > 3:
+        exit()
+    for item in board:
+        
+    
+    
