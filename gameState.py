bcc19350b0de1220d59a9f321606790b030692f3
Christopher G Dolan <cdolan@mit.edu>
2011-08-17 21:38:59 -0700

    Added

---
+++ gameState.py
@@ -0,0 +1,12 @@
+class Gem:
+    def __init__(self, color, status):
+        self.color = color
+        self.status = status
+
+class Board:
+    def __init__(self):
+        self.board = [][]
+    
+    
+    
+
